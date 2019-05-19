c3703dccc1c8fd4daf3c92a2ad7a94b63b2c85e0
“[Jasmine <jazzykitkat@windowslive.com>
2019-05-19 20:20:33 +1200

    no message

---
+++ Scorching_Cold_Mini/Source/Scorching_Cold_Mini/Scorching_Cold_Mini.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Scorching_Cold_Mini.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Scorching_Cold_Mini, "Scorching_Cold_Mini" );
+ 
